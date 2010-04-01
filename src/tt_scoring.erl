d99b9dd1e74801459294b1f1ec8784e3ff6325ae
Torbjorn Tornkvist <tobbe@tornkvist.org>
2010-04-01 14:27:21 +0200

    playng with scoring rules

--- src/tt_scoring.erl
+++ src/tt_scoring.erl
@@ -14,7 +14,7 @@
 %% @spec v1(Winner::integer(), Looser::integer()) -> {Winner,Looser}.
 
 v1(W,L) when W>L -> {W+(1/(W-L)), L-(1/((W-L)*2))};
-v1(W,L) when W<L -> {W+log10((L-W)*2), L-log10(L-W)};
+v1(W,L) when W<L -> {W+log10((L-W)*3), L-log10(L-W)};
 v1(W,L)          -> {W, L}.
 
 
