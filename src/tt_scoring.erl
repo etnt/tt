64f5a0005903ede3c62dfc4bc599e915483fca44
Jordi Chacon <jordi.chacon@gmail.com>
2010-04-24 16:53:16 +0200

    Improved scoring system. Now it's more fair

--- src/tt_scoring.erl
+++ src/tt_scoring.erl
@@ -13,8 +13,22 @@
 %% @doc Simple scoring model
 %% @spec v1(Winner::integer(), Looser::integer()) -> {Winner,Looser}.
 
-v1(W,L) when W>L -> {W+(1/(W-L)), L-(1/((W-L)*2))};
-v1(W,L) when W<L -> {W+log10((L-W)*3), L-log10(L-W)};
-v1(W,L)          -> {W+0.1, L-0.1}.
-
+v1(W, L) when W > L -> 
+    {W + min(1, (1 / (W - L))), L - min(0.7, (1 / ((W - L) * 2)))};
+v1(W, L) when W < L -> 
+    {W + log10(max(10, (L - W) * 3)), L - log10(max(7, L - W))};
+v1(W, L) -> 
+    {W + 1, L - 1}.
     
+min(A, B) ->
+    case A < B of
+	true  -> A;
+	false -> B
+    end.
+	    
+max(A, B) ->
+    case A > B of
+	true  -> A;
+	false -> B
+    end.
+	    
